ac1e2e2c81c3fa8cc6520690183333f6d874556d
Lars Wikman <lars@underjord.io>
2024-08-21 22:07:19 +0200

    update nerves_hub_link

--- mix.exs
+++ mix.exs
@@ -104,7 +104,7 @@ defmodule Kiosk.MixProject do
       {:nerves_system_x86_64, "~> 1.24", runtime: false, targets: :x86_64},
       {:nerves_system_grisp2, "~> 0.8", runtime: false, targets: :grisp2},
       {:nerves_system_mangopi_mq_pro, "~> 0.6", runtime: false, targets: :mangopi_mq_pro},
-      {:nerves_hub_link, "~> 2.4"},
+      {:nerves_hub_link, "~> 2.5"},
       {:nerves_hub_cli, "~> 2.0"},
       {:frio_rpi4,
        path: "../frio_rpi4", runtime: false, nerves: [compile: true], targets: :frio_rpi4},
