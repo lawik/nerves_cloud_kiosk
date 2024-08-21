ecf01c5b65820bf086f1f892c62f916bd2badabe
Lars Wikman <lars@underjord.io>
2024-08-21 20:20:56 +0200

    Some UI stuff

--- kiosk_ui/lib/kiosk_ui_web/live/onboard_live.ex
+++ kiosk_ui/lib/kiosk_ui_web/live/onboard_live.ex
@@ -20,13 +20,25 @@ defmodule KioskUiWeb.OnboardLive do
 
     <div :if={@network_up? and @link_set_up?} class="px-4 py-2 rounded-full bg-slate-300">The link to NervesHub has been set up.</div>
     <div :if={@network_up? and not @link_set_up?}>
-        <h2 class="bold text-xl mb-8">Connect NervesHub link</h2>
+        <h2 class="bold text-xl mb-8">Connect to NervesHub</h2>
         <p>Bring this device into your NervesHub account with a Shared Secret for the easiest onboarding.</p>
-        <form id="nh_link" name="nh_link" phx-submit="submit_link" class="flex flex-wrap">
-            <label class="block grow">NervesHub instance <input type="text" id="nh_instance" name="nh_instance" value="devices.nervescloud.com" phx-update="ignore" /></label>
-            <label class="block grow">Serial number <input type="text" id="nh_identifier" name="nh_identifer" value={System.unique_integer([:positive])} phx-update="ignore" /></label>
-            <label class="block grow">Product key <input type="text" id="nh_key" name="nh_key" /></label>
-            <label class="block grow">Product secret <input type="text" id="nh_secret" name="nh_secret" /></label>
+        <form id="nh_link" name="nh_link" phx-submit="submit_link" class="flex flex-wrap justify-center gap-4">
+            <label class="block w-full flex flex-wrap justify-center">
+              <span class="block w-full text-center">NervesHub instance</span>
+              <input class="block w-full rounded-full bg-slate-200 border-0 text-center" type="text" id="nh_instance" name="nh_instance" value="devices.nervescloud.com" phx-update="ignore" />
+            </label>
+            <label class="block w-full flex flex-wrap justify-center">
+              <span class="block w-full text-center">Serial number</span>
+              <input class="block w-full rounded-full bg-slate-200 border-0  text-center" type="text" id="nh_identifier" name="nh_identifer" value={System.unique_integer([:positive])} phx-update="ignore" />
+            </label>
+            <label class="block w-full flex flex-wrap justify-center">
+              <span class="block w-full text-center">Product key</span>
+              <input class="block w-full rounded-full bg-slate-200 border-0  text-center" type="text" id="nh_key" name="nh_key" />
+            </label>
+            <label class="block w-full flex flex-wrap justify-center">
+              <span class="block w-full text-center">Product secret</span>
+              <input class="block w-full rounded-full bg-slate-200 border-0  text-center" type="text" class="grow" id="nh_secret" name="nh_secret" />
+            </label>
             <button>Connect</button>
         </form>
     </div>
